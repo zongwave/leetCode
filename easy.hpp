438f603e61040f140859c32a022b1cff6d19cb71
zongwave <zongwave@hotmail.com>
2021-11-22 10:25:04 +0800

    easy: Number of 1 Bits

--- easy.hpp
+++ easy.hpp
@@ -636,5 +636,26 @@ public:
     }
 };
 
+/* 191. Number of 1 Bits
+*
+* Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+*     Constraints:
+*         The input must be a binary string of length 32.
+*/
+class HammingWeight {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+
+        while (n > 0) {
+            if (n & 0x01) {
+                count += 1;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+};
+
 #endif
 

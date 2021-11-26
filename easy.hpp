f71f0e46e6ca6b41c572d89e71bd6c966df6429c
zongwave <zongwave@hotmail.com>
2021-11-26 15:33:47 +0800

    easy: Pascal's Triangle

--- easy.hpp
+++ easy.hpp
@@ -845,5 +845,43 @@ public:
     }
 };
 
+/* 118. Pascal's Triangle
+*
+*    Given an integer numRows, return the first numRows of Pascal's triangle.
+*    In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+*
+*    Example 1:
+*        Input: numRows = 5
+*        Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+*
+*    Constraints:
+*        1 <= numRows <= 30
+*/
+class PascalTriangle {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle;
+        triangle.resize(numRows);
+        triangle[0] = { 1 };
+
+        if (numRows > 1) {
+            triangle[1] = { 1, 1 };
+
+            for (int num = 2; num < numRows; num++) {
+                vector<int> row;
+                row.resize(num + 1);
+                row[0] = 1;
+                row[num] = 1;
+                for (int idx = 1; idx < num; idx++) {
+                    row[idx] = triangle[num - 1][idx - 1] + triangle[num - 1][idx];
+                }
+                triangle[num] = row;
+            }
+        }
+
+        return triangle;
+    }
+};
+
 #endif
 
